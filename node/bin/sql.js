9b6c26e080cee20a86cc713f83d58c57bdfb2848
bjtuhbxy <158299671@qq.com>
2019-02-16 20:09:13 +0800

    +增加jwt认证

---
+++ node/bin/sql.js
@@ -0,0 +1,6 @@
+const UserSQL = {
+    insert: 'INSERT INTO User(uid,userName) VALUES(?,?)',
+    queryAll: 'SELECT * FROM User',
+    getUserById: 'SELECT * FROM user WHERE name = ? ',
+};
+module.exports = UserSQL;
