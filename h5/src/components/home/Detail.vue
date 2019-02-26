69675882e822225e33f2de51f91bacf275304e3e
weiwei5340 <vweiwei1@163.com>
2019-02-26 17:27:13 +0800

    *

--- h5/src/components/home/Detail.vue
+++ h5/src/components/home/Detail.vue
@@ -1,32 +1,70 @@
 <template>
-  <div class="page-navbar">
-    <button class="iconfont icon-htmal5icon37" @click="$router.go(-1)">返回</button>
-    <div class="page-title">
-      <a href="#" :class="{active:selected == 'sp'}">商品</a>
-      <a href="#">评价</a>
-      <a href="#">详情</a>
+  <div>
+    <!--nav-->
+    <div class="page-navbar">
+      <button class="iconfont icon-htmal5icon37" @click="$router.go(-1)">返回</button>
+      <div class="page-title">
+        <a href="javascript:;" v-for="(item,index) in pageClass" @click="pageSlide" :key="index" :name="item.id"
+           :class="{active:selected == item.id}">{{item.label}}</a>
+      </div>
+      <span class="iconfont icon-gengduo"></span>
+    </div>
+    <div>
+      <img id="firstImg" onload="window._PFM_TIMING[4] = window._PFM_TIMING[4] || new Date();"
+           src="//m.360buyimg.com/mobilecms/s750x750_jfs/t1/32251/26/438/89633/5c3c055bE61414b4d/5691c8e1a9f05df9.jpg!q80.dpg.webp"
+           onerror="__reloadResource(this)">
     </div>
   </div>
+  <!--main-->
 </template>
 
 <script>
   export default {
     name: 'Detail',
-    data () {
+    data() {
       return {
-        selected:'sp',
+        selected: 'goods',
+        pageClass: [
+          {
+            id: 'goods',
+            label: '商品'
+          }, {
+            id: 'evaluate',
+            label: '评价'
+          }, {
+            id: 'detail',
+            label: '详情'
+          }
+        ]
       }
     },
-    components:{
-    },
-    methods:{
+    components: {},
+    methods: {
+      pageSlide: function (event) {
+        this.selected = event.target.name
+        console.log(event);
+      }
     },
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .active{
-    color: #fff;
+  .page-title > a {
+    font-size: 0.4rem;
+    margin: 0 0.1rem;
+    color: #ddd;
+  }
+
+  .active {
+    color: #fff !important;
+    font-size: 0.45rem !important;
+  }
+
+  .icon-gengduo {
+    position: absolute;
+    right: 14px;
+    top: 14px;
+    color: #ddd;
   }
 </style>
