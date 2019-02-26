062526896750d442e776f6948a25fc5a012c0f65
bjtuhbxy <158299671@qq.com>
2019-02-26 15:06:38 +0800

    *

--- h5/src/components/home/Detail.vue
+++ h5/src/components/home/Detail.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="page-navbar">
     <button class="iconfont icon-htmal5icon37" @click="$router.go(-1)">返回</button>
-    <div class="page-title" v-model="selected">
-      <a href="#">商品</a>
+    <div class="page-title">
+      <a href="#" :class="{active:selected == 'sp'}">商品</a>
       <a href="#">评价</a>
       <a href="#">详情</a>
     </div>
@@ -14,7 +14,7 @@
     name: 'Detail',
     data () {
       return {
-        selected:'商品',
+        selected:'sp',
       }
     },
     components:{
@@ -26,5 +26,7 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    
+  .active{
+    color: #fff;
+  }
 </style>
