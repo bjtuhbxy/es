1c340a453722031b381fc66c009397328118bca0
bjtuhbxy <158299671@qq.com>
2019-02-26 12:12:11 +0800

    *调整样式

--- h5/src/components/home/Order.vue
+++ h5/src/components/home/Order.vue
@@ -1,23 +1,6 @@
 <template>
-  <div>
-    <mt-navbar v-model="selected">
-      <mt-tab-item id="1">option A</mt-tab-item>
-      <mt-tab-item id="2">option B</mt-tab-item>
-      <mt-tab-item id="3">option C</mt-tab-item>
-    </mt-navbar>
-
-    <!-- tab-container -->
-    <mt-tab-container v-model="selected">
-      <mt-tab-container-item id="1">
-        <mt-cell v-for="n in 10" :title="'content ' + n" />
-      </mt-tab-container-item>
-      <mt-tab-container-item id="2">
-        <mt-cell v-for="n in 4" :title="'content ' + n" />
-      </mt-tab-container-item>
-      <mt-tab-container-item id="3">
-        <mt-cell v-for="n in 6" :title="'content ' + n" />
-      </mt-tab-container-item>
-    </mt-tab-container>
+  <div class="page-navbar">
+    <div class="page-title">订单</div>
   </div>
 </template>
 
