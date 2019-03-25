54787f83c976aab89ef46a447aac9d9ec2e3ff7b
bjtuhbxy <158299671@qq.com>
2019-03-26 05:23:50 +0800

    *

--- web/src/components/homeLeft.vue
+++ web/src/components/homeLeft.vue
@@ -3,7 +3,7 @@
     <ul>
       <li>接口列表</li>
       <li v-for="(item,index) in ul" :key="index">
-        {{item.api}}
+        {{index + 1}}-{{item}}
       </li>
     </ul>
   </div>
@@ -14,14 +14,21 @@ export default {
   name: 'homeLeft',
   data () {
     return {
-      ul: [
-        {
-          api: 'users'
-        },
-        {
-          api: 'goods'
-        }
-      ]
+      ul: []
+    }
+  },
+  mounted () {
+    this.test()
+  },
+  methods: {
+    test: function () {
+      let url = '/api/home/getList'
+      var vm = this
+      this.$http.get(url).then(function (res) {
+        vm.ul = res.data
+      }).catch(function (err) {
+        console.log(err)
+      })
     }
   }
 }
--- web/src/main.js
+++ web/src/main.js
@@ -7,8 +7,14 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // 引入路由
 import router from './router'
+// 引入http axios框架
+import axios from 'axios'
+// 扩展vue原型方法
+Vue.prototype.$http = axios
 // 使用ele-ui
 Vue.use(ElementUI)
+// 使用axios框架
+// Vue.use(axios)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
