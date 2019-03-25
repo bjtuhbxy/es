9fa7a0f342f220f61030568e22a2ee1b5c59b351
bjtuhbxy <158299671@qq.com>
2019-03-25 13:48:47 +0800

    初始化新分支

--- node/bin/sql.js
+++
@@ -1,6 +0,0 @@
-const UserSQL = {
-    insert: 'INSERT INTO User(uid,userName) VALUES(?,?)',
-    queryAll: 'SELECT * FROM User',
-    getUserById: 'SELECT * FROM user WHERE name = ?',
-};
-module.exports = UserSQL;
